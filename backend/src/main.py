5d8c3cfa147e5a0f7fa80bef3eb484c1a5ee31f6
ghafarahmad635 <ghafarahmad635@gmail.com>
2025-07-23 08:16:21 +0500

    updated main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+import os
+API_KEY = os.getenv("API_KEY")
+if( not API_KEY):
+    raise NotImplementedError("API_KEY is not set in the environment variables")
 app=FastAPI()
 @app.get("/")
 def read_root():
